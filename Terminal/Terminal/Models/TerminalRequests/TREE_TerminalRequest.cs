2e9c135b30cdbf0b03e711c0ce54e36c94235c05
vtsoma <cheesetkh39@gmail.com>
2023-05-25 13:40:40 +0300

    Add base file for tree

---
+++ Terminal/Terminal/Models/TerminalRequests/TREE_TerminalRequest.cs
@@ -0,0 +1,18 @@
+﻿using Terminal.Handlers;
+using Terminal.Helpers;
+using Terminal.Models.TerminalRequests.Base;
+
+namespace Terminal.Models.TerminalRequests
+{
+    internal class TREE_TerminalRequest : TerminalRequest
+    {
+        public TREE_TerminalRequest() {
+            CommandName = "tree";
+        }
+
+        public override void Execute(CommandHandler handler, string commandBody = "")
+        {
+            base.Execute(handler, commandBody);
+        }
+    }
+}
