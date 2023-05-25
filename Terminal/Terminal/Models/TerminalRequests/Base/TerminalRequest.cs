392f24d2d8536c25342a60196eaa792c06203c1b
Dmitry Husak <dmygus@.gmail.com>
2023-05-25 12:54:26 +0300

    added project

---
+++ Terminal/Terminal/Models/TerminalRequests/Base/TerminalRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terminal.Handlers;
+
+namespace Terminal.Models.TerminalRequests.Base
+{
+    public class TerminalRequest
+    {
+        public string CommandName { get; protected set; }
+        public virtual void Execute(CommandHandler handler, string commandBody = "") 
+        {
+            Console.WriteLine($"Executed: {CommandName} {commandBody}");
+        }
+    }
+}
---
+++ Terminal/Terminal/Models/TerminalRequests/CD_TerminalRequest.cs
@@ -0,0 +1,44 @@
+﻿using Terminal.Handlers;
+using Terminal.Helpers;
+using Terminal.Models.TerminalRequests.Base;
+
+namespace Terminal.Models.TerminalRequests
+{
+    internal class CD_TerminalRequest : TerminalRequest
+    {
+        public CD_TerminalRequest()
+        {
+            CommandName = "cd";
+        }
+        public override void Execute(CommandHandler handler, string commandBody = "")
+        {
+            try
+            {
+                switch (commandBody)
+                {
+                    case "":
+              
+                        throw new Exception("Enter command body");
+                    case "..":
+                        if (!handler.SetCurrentDirectory(CommandHandler.CurrentDirectoryPath.Substring(0, CommandHandler.CurrentDirectoryPath.LastIndexOf('\\')))) throw new Exception("Directory not exists");
+                        break;
+                    default:
+                        if (!handler.SetCurrentDirectory(commandBody)) throw new Exception("Directory not exists");
+                        break;
+                }
+                //if (commandBody == "") throw new Exception("Enter command body");
+                //if (commandBody == "..")
+                //{
+                //    if (!handler.SetCurrentDirectory(CommandHandler.CurrentDirectoryPath.Substring(0, CommandHandler.CurrentDirectoryPath.LastIndexOf('\\')))) throw new Exception("Directory not exists");
+                //    return;
+                //}
+                //if (!handler.SetCurrentDirectory(commandBody)) throw new Exception("Directory not exists");
+
+            }
+            catch (Exception ex)
+            {
+                ConsoleHelper.WriteColorLine($"Error: {ex.Message}", ConsoleColor.DarkRed);
+            }
+        }
+    }
+}
