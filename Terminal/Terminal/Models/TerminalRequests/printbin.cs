48ad88abe76fc2919c841e87082f30f9b1edddfa
4ort <abonetetz@gmail.com>
2023-05-25 15:01:17 +0300

    buba

---
+++ Terminal/Terminal/Models/TerminalRequests/printbin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terminal.Handlers;
+using Terminal.Models.TerminalRequests.Base;
+
+namespace Terminal.Models.TerminalRequests
+{
+    internal class printbin : TerminalRequest
+    {
+        public printbin()
+        {
+            CommandName = "printbin";
+        }
+        public override void Execute(CommandHandler handler, string commandBody = "")
+        {
+            Console.WriteLine("Lox");
+        }
+    }
+}
