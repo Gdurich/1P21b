43339efa96c00bd3e703c8e903ec634b8a82a0b9
Maxoonzoo <maxoonzoo1@gmail.com>
2023-05-26 08:24:17 +0300

    added code for tree but path fucking do not rabotaet

--- Terminal/Terminal/Models/TerminalRequests/TREE_TerminalRequest.cs
+++ Terminal/Terminal/Models/TerminalRequests/TREE_TerminalRequest.cs
@@ -10,8 +10,31 @@ namespace Terminal.Models.TerminalRequests
             CommandName = "tree";
         }
 
+
         public override void Execute(CommandHandler handler, string commandBody = "")
         {
+                string path = CurrentDirectoryPath;
+                int indentSize = 4;
+                printTree(path, indentSize, "");
+
+            static void printTree(string path, int indentSize, string indent)
+            {
+                DirectoryInfo directory = new DirectoryInfo(path);
+
+                Console.WriteLine(indent + directory.Name);
+
+                FileInfo[] files = directory.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    Console.WriteLine(indent + new string(' ', indentSize) + file.Name);
+                }
+
+                DirectoryInfo[] subDirectories = directory.GetDirectories();
+                foreach (DirectoryInfo subDirectory in subDirectories)
+                {
+                    printTree(subDirectory.FullName, indentSize, indent + new string(' ', indentSize));
+                }
+            }
             base.Execute(handler, commandBody);
         }
     }
