e9f8af38ae040517d88f682c9729ff1bb7057eea
CMD <abonetetz@gmail.com>
2023-06-04 17:41:30 +0300

    biba

--- Terminal/Terminal/Program.cs
+++ Terminal/Terminal/Program.cs
@@ -21,9 +21,14 @@ void Init()
         .AddCommandObject(new CD_TerminalRequest());
     commandHandler
         .AddCommandObject(new printbin());
-        .AddCommandObject(new CD_TerminalRequest())
-        .AddCommandObject(new Append())
-        .AddCommandObject(new TREE_TerminalRequest())
-        .AddCommandObject(new REPLACE_TerminalRequest())
+    commandHandler
+        .AddCommandObject(new CD_TerminalRequest());
+    commandHandler
+        .AddCommandObject(new Append());
+    commandHandler
+        .AddCommandObject(new TREE_TerminalRequest());
+    commandHandler
+        .AddCommandObject(new REPLACE_TerminalRequest());
+    commandHandler
         .AddCommandObject(new REWRITE_TerminalRequest());
 }
