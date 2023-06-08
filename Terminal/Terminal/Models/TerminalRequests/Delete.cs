bf71f19f4f7209771a61779d9e0a2b662928d98c
Hohlov Kostiantyn <lookop231@gmail.com>
2023-06-08 18:57:20 +0300

    delete

---
+++ Terminal/Terminal/Models/TerminalRequests/Delete.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Terminal.Handlers;
+using Terminal.Models.TerminalRequests.Base;
+
+namespace Terminal.Models.TerminalRequests
+{
+    internal class Delete : TerminalRequest
+    {
+        public Delete() 
+        { 
+            CommandName = "delete";
+        }
+
+        public override void Execute(CommandHandler handler, string commandBody = "")
+        {
+            Console.WriteLine("Введіть назву файлу або папки, яку потрібно видалити:");
+            string name = Console.ReadLine();
+
+            if (File.Exists(name))
+            {
+                File.SetAttributes(name, FileAttributes.Normal);
+                File.Delete(name);
+                Console.WriteLine($"Файл {name} успішно видалено.");
+            }
+            else if (Directory.Exists(name))
+            {
+                if (Directory.GetFileSystemEntries(name).Length > 0)
+                {
+                    Console.WriteLine($"Папка {name} не може бути видалена, оскільки вона містить інші файли або папки.");
+                }
+                else
+                {
+                    Directory.Delete(name);
+                    Console.WriteLine($"Папку {name} успішно видалено.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Файл або папку '{name}' не знайдено.");
+            }
+        }
+    }
+}
+
+
--- Terminal/Terminal/Program.cs
+++ Terminal/Terminal/Program.cs
@@ -19,4 +19,6 @@ void Init()
 {
     commandHandler
         .AddCommandObject(new CD_TerminalRequest());
+    commandHandler
+       .AddCommandObject(new Delete());
 }
